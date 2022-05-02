980816da2f92a613c7753abae118d05a41bd293d
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2022-05-02 06:56:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+sk1 = int(input('Ivadi pirmo skaitlu:'))
+sk2 = int(input('Ievadi otro skaitlu:'))
+if sk1 >= sk2 or sk1 == sk2:
+  print(sk1 + sk2)
+else:
+  print(sk2 * sk1)
+  print('Uzdevums paveikts!')
